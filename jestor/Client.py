57b77a7df33f78cc4e9baa01cad9b3f46131e664
Renato Menegasso <renato.menegasso@jestor.com>
2022-08-29 18:56:29 -0300

    Removing uneeded host header

--- jestor/Client.py
+++ jestor/Client.py
@@ -13,7 +13,6 @@ class Client:
             headers = {}
             headers["Accept"] = "application/json"
             headers["Authorization"] = f'Bearer {self.token}'
-            headers["Host"] = f'https://{self.org}.api.jestor.com'
             
             url = f'https://{self.org}.api.jestor.com/v3/low-code-execution/{path}'
             
